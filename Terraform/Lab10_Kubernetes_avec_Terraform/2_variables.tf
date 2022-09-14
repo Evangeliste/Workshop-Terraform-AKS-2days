3eb9ae9fb48a61aa74ea49d431d02b8e20a09e48
Evangeliste <alexis@inventiq.fr>
2022-09-14 16:19:54 +0200

    Lab 10

--- Terraform/Lab10_Kubernetes_avec_Terraform/2_variables.tf
+++ Terraform/Lab10_Kubernetes_avec_Terraform/2_variables.tf
@@ -1,11 +1,11 @@
 #  Resource Group Name du cluster AKS
 variable "resource_group_name" {
   type    = string
-  default = "RG-AKSCluster"
+  default = "RG-ACE-001"
 }
 
 # AKS Cluster name
 variable "cluster_name" {
   type    = string
-  default = "AKS-000"
+  default = "AKS-ACE-001"
 }
